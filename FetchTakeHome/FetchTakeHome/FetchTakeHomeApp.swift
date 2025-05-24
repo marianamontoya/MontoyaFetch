5295bf04176aa0310aa038f09cd13ccd82eceefe
Mariana Montoya <marianamontoya1120@gmail.com>
2025-05-24 16:46:45 -0700

    Add files via upload
    Uploading initial files

---
+++ FetchTakeHome/FetchTakeHome/FetchTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeApp.swift
+//  FetchTakeHome
+//
+//  Created by Mariana Montoya on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
